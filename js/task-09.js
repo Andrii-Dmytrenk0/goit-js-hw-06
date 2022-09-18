9546e131cd6a2d4a29092ecbd31648e060eae58f
Andrii-Dmytrenk0 <dmitrenko1567@gmail.com>
2022-09-18 20:36:16 +0200

    app 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', onColorChange);
+
+function onColorChange(event) {
+  const color = document.querySelector('body').style.backgroundColor = getRandomHexColor();
+  document.querySelector('.color').textContent = color;
+};
