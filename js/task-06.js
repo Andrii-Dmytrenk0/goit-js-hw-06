74741e6a06fd93f95af30258f69117ac481ee418
Andrii-Dmytrenk0 <dmitrenko1567@gmail.com>
2022-09-18 16:35:21 +0200

    fix task 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,11 @@ const input = document.querySelector('#validation-input');
 
 input.addEventListener('blur', () => {
     if (input.value.length > input.getAttribute('data-length') || input.value.length < input.getAttribute('data-length')) {
+        input.classList.remove('valid');
         input.classList.add('invalid');
-    };
-    input.classList.add('valid');
+    }
+    else {
+        input.classList.remove('invalid');
+        input.classList.add('valid');
+    }
 });
